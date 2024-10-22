ec28a8b1a4a221e88d3d1865465312eb3d528cff
ZXINNATTAPAT <bigboyzin88@gmail.com>
2024-10-22 22:38:26 +0700

    Update settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -142,13 +142,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "https://olafs.netlify.app"
 ]
 CORS_ALLOW_CREDENTIALS = True
 CSRF_COOKIE_SECURE = True
 CSRF_COOKIE_HTTP_ONLY = True
 CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "https://olafs.netlify.app"
 ]
 CORS_EXPOSE_HEADERS = ["Content-Type", "X-CSRFToken"]
 SESSION_COOKIE_SECURE = True
