483ac732b05a4ed05642bd21d5c0e24dcc2a47ef
ZXINNATTAPAT <bigboyzin88@gmail.com>
2024-10-22 17:09:23 +0700

    try to setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -129,6 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [     # ไดเรกทอรีที่ใช้เก็บไฟล์ static ในระหว่างการพัฒนา
+    BASE_DIR / "static",
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"  # ไดเรกทอรีที่ใช้เก็บไฟล์ static สำหรับ production
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
